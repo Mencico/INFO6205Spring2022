18b11569c03dc5e75f1421422c47b87c4a1db223
Min Zhang <zhang.min8@northeastern.edu>
2022-02-13 23:38:41 -0800

    This is my assignment3. Thank you so much!

---
+++ Assignment 3/Q3.java
@@ -0,0 +1,42 @@
+import java.util.PriorityQueue;
+
+public class Q3 {
+
+    class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+
+    class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            PriorityQueue<ListNode> heap=new PriorityQueue<>((a, b)-> a.val-b.val);
+            ListNode head=null;
+            ListNode cur=null;
+            for(ListNode list:lists){
+                if(list!=null){
+                    heap.add(list);
+                }
+            }
+            while(!heap.isEmpty()){
+                ListNode list=heap.poll();
+                if(list.next!=null){
+                    heap.add(list.next);
+                }
+
+                if(head==null){
+                    head=list;
+                    cur=head;
+                }else{
+                    cur.next=list;
+                    cur=list;
+                }
+
+            }
+            return head;
+        }
+    }
+}
