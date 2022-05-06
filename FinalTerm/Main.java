7516b4f4db776b0ef997f22b4a4ea743ea25e415
Min Zhang <zhang.min8@northeastern.edu>
2022-05-06 10:55:26 -0700

    This is my final test. Thank you so much!

---
+++ FinalTerm/Main.java
@@ -0,0 +1,11 @@
+public class Main {// This class just for test case.
+
+    public static void main(String[] args) {
+        Q1 test = new Q1();
+        System.out.println(test.compressString("aabcccccaaa"));
+        System.out.println(test.compressString("ab"));//// Question1 is AC!
+
+    }
+
+
+}
